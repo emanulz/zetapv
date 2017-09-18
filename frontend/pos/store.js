7b39d1b188f7481344e7a06c0c57e1210c91b285
Emanuel Zúñiga Infante <emanuzuniga@gmail.com>
2017-09-18 06:12:11 -0600

    0.0.43 Prod build for show
    Prod build for show

--- frontend/pos/store.js
+++ frontend/pos/store.js
@@ -1,11 +1,11 @@
-import { applyMiddleware, createStore } from "redux"
+import { applyMiddleware, createStore } from 'redux'
 
-import logger from "redux-logger"
-import thunk from "redux-thunk"
-import promise from "redux-promise-middleware"
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise-middleware'
 
-import reducer from "./reducer"
+import reducer from './reducer'
 
-const middleware = applyMiddleware(promise(), thunk, logger)
+const middleware = applyMiddleware(promise(), thunk)
 
 export default createStore(reducer, middleware)
